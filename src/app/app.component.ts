fd07cf25ee39a8aad6f39a16a8d8caf4287e0e8d
forderation <kharisma.muzaki@gmail.com>
2019-03-25 00:47:30 +0700

    modul2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kharisma Muzaki Ghufron';
-  panjang : Number = 10;
-  lebar : Number = 10;
-  isBreakTime : Boolean = true;
+  title = '';
+  panjang: Number = 10;
+  lebar: Number = 10;
+  isBreakTime: Boolean = true;
+  isLoggedIn: Boolean = true;
 
-  handleBlur(event: any){
-    console.log(event.target.value);
+  handleBlur(event: any) {
+    this.title = event.target.value;
   }
+
+  handleInput(event: any) {
+    this.title = event.target.value;
+  }
+
+  handleClick(event: any) {
+    this.title = "Kharisma Muzaki Ghufron";
+  }
+
+  handleChange(value){
+    this.title = value;
+  }
+
+  todos: any = [
+    {
+      id:1,
+      todo:"Create module perulangan",
+      status:true,
+      createdAt: new Date(2019,3,20)
+    },
+    {
+      id:2,
+      todo:"Create example project",
+      status:false,
+      createdAt: new Date(2019,3,21)
+    },
+    {
+      id:3,
+      todo:"Create dummy backend",
+      status:false,
+      createdAt: new Date(2019,3,22)
+    },
+    {
+      id:4,
+      todo:"Create challenge code",
+      status:false,
+      createdAt: new Date(2019,3,23)
+    }
+  ];
 }
